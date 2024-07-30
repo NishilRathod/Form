a4f759f548eed966d6c39da07a07d85b4b595ccd
NishilRathod <nishilrathod7105@gmail.com>
2024-07-30 17:18:15 +0530

    css changes and added button disbaled function

--- submit.js
+++ submit.js
@@ -1,3 +1,36 @@
+    let inputs = document.querySelectorAll('input');
+    let buttonSend1 = document.getElementById('button1');
+    let buttonSend2 = document.getElementById('button2');
+    let inputValidator = {
+    "username": false,
+    "email": false,
+    "phoneno": false,
+    "age":false,
+    "gender": false
+    }
+
+inputs.forEach((input) => {
+  input.addEventListener('change', (event) => {
+    let name = event.target.name;
+    if (event.target.value.length > 0) {
+      inputValidator[name] = true;
+    } else {
+      inputValidator[name] = false;
+    };
+
+    let allTrue = Object.keys(inputValidator).every((item) => {
+      return inputValidator[item] === true
+    });
+
+    if (allTrue) {
+      buttonSend1.disabled = false;
+      buttonSend2.disabled = false;
+    } else {
+      buttonSend1.disabled = true;
+      buttonSend2.disabled = true;
+    }
+  })
+})
 
 function openPopup(){
     let popup = document.getElementById("popup1");
